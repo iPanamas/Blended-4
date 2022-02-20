fda8f458e86198065e7be9acffa54a107c6bfd4c
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-02-20 12:53:16 +0200

    add localStorage

--- src/js/note-list.js
+++ src/js/note-list.js
@@ -2,7 +2,8 @@ const refs = {
   noteList: document.querySelector('.list-group'),
   form: document.getElementById('form-js'),
 };
-const noteArray = [];
+const noteArray = JSON.parse(localStorage.getItem('form-data')) ?? [];
+console.log(JSON.parse(localStorage.getItem('form-data')));
 
 const createMarkup = ({ url, title, description }) => {
   return `<li class="list-group-item">
@@ -12,14 +13,27 @@ const createMarkup = ({ url, title, description }) => {
   </li>`;
 };
 
+const renderMarkup = () => {
+   refs.noteList.innerHTML = noteArray.map(createMarkup).join('');
+  
+};
+renderMarkup();
+
+
+
 const submitForm = event => {
   event.preventDefault();
   const { url, title, description } = event.currentTarget.elements;
   noteArray.push({ url: url.value, title: title.value, description: description.value });
 
-  const renderMarkup = noteArray.map(createMarkup).join('');
+  // const renderMarkup = noteArray.map(createMarkup).join('');
 
-  refs.noteList.innerHTML = renderMarkup;
+
+  // refs.noteList.innerHTML = renderMarkup();
+  renderMarkup();
+
+  localStorage.setItem('form-data', JSON.stringify(noteArray));
 };
 
 refs.form.addEventListener('submit', submitForm);
+
